25957db9f694537334aaf2df1ade3f1dd901bf84
Fatoumata Mandioula Diallo <fatoumatamandioula1@gmail.com>
2026-04-28 16:53:19 +0000

    Lab 2 : composants statiques LignesBus et ListeLignes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import './App.css';
 import Header from './Header';
+import ListeLignes from './ListeLignes';
 import Footer from './Footer';
-import Stat1 from './Statistique1';
-import Stat2 from './Statistique2';
-import Stat3 from './Statistique3';
+
 function App() {
+  const lignes = [
+    { id: 1, numero: "1", depart: "Parcelles Assainies",arrivee: "Plateau",arrets: 14},
+
+    { id: 2, numero: "7", depart: "Guediawaye",arrivee: "Place Obe", arrets: 18},
+
+    { id: 3, numero: "15", depart: "Pikine",arrivee: "Medina",arrets: 12 },
+
+    { id: 4, numero: "23", depart: "Ouakam",arrivee: "Grand Dakar", arrets: 10 },
+
+    { id: 5, numero: "8", depart: "Almadies",arrivee: "Colobane",arrets: 16 },
+
+    { id: 6, numero: "12", depart: "Yoff",arrivee: "Sandaga",arrets: 11 },
+  ];
   return (
     <div className="App">
-     <Header />
+      <Header />
       <main className="contenu">
-       <p>Bienvenue ! Cette application vous aide a trouver
-         votre ligne de bus a Dakar.
-       </p>
-
-        <h2>Statistiques du réseau DDD</h2>
-        <div style={{ display: 'flex', gap: '20px', justifyContent: 'center' }}>
-          <Stat1 />
-          <Stat2 />
-          <Stat3 />
-        </div>
+        <ListeLignes lignes={lignes} />
       </main>
-      
       <Footer />
-
     </div>
   );
 }
